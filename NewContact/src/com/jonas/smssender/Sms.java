1a7801e7f4b76e0fc0b4e4631e235f74c977b3c2
PenguinMore <PenguinMore@60587213-1f97-928a-cde8-6f4b2d768771>
2011-03-11 15:11:32 +0000

    a new Contact branch

---
+++ NewContact/src/com/jonas/smssender/Sms.java
@@ -0,0 +1,71 @@
+package com.jonas.smssender;
+import com.jonas.smssender.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+
+
+
+public class Sms extends Activity {
+	/** Called when the activity is first created. */
+	private ListView list;
+	private static final String DBNAME = "SMS";
+	private static final int VERSION = 1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sms);
+		// create new DBhelper instance
+		DBHelper dbhelper = new DBHelper(this, DBNAME, null, VERSION);
+		SQLiteDatabase db = dbhelper.getWritableDatabase();
+
+		list = (ListView) this.findViewById(R.id.ListView01);
+		list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+		list.setFocusable(false);
+		list.setOnItemClickListener(new OnItemClickListener() {
+
+			// get the message in selected ListView
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+
+				Bundle selectedMsg = new Bundle();
+				selectedMsg.putString("Message", ((TextView) arg1).getText()
+						.toString());
+
+				Intent intent = new Intent();
+				intent.putExtras(selectedMsg);
+				intent.setClass(Sms.this, Send.class);
+				startActivity(intent);
+				Sms.this.finish();
+
+			}
+		});
+
+		Cursor cur = db.query(DBHelper.TB_name, new String[] { DBHelper.ID,
+				DBHelper.MESSAGE }, null, null, null, null, null);
+
+		if (cur != null && cur.getCount() >= 0) {
+			ListAdapter adapter = new SimpleCursorAdapter(this,
+					android.R.layout.simple_list_item_1, cur,
+					new String[] { DBHelper.MESSAGE },
+					new int[] { android.R.id.text1 });
+			list.setAdapter(adapter);
+		}
+		dbhelper.close();
+
+	}
+
+}
+    
