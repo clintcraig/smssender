837709d876119861c4a26f28fa8983dd9c76d389
PenguinMore <PenguinMore@60587213-1f97-928a-cde8-6f4b2d768771>
2011-03-28 06:28:57 +0000

    add AppWidget.java for appwidget function appwidget_info.xml for appwidget info widget.xml for appwidget layout

---
+++ trunk/appWidget/src/com/jonas/smssender/AppWidget.java
@@ -0,0 +1,61 @@
+package com.jonas.smssender;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.SmsManager;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+public class AppWidget extends AppWidgetProvider {
+    private String result;
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		int resultCode = getResultCode();
+
+		switch (resultCode) {
+		case Activity.RESULT_OK:
+			result = "Message Sent";
+			break;
+		case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+
+			break;
+		case SmsManager.RESULT_ERROR_NO_SERVICE:
+
+			break;
+		case SmsManager.RESULT_ERROR_NULL_PDU:
+
+			break;
+		case SmsManager.RESULT_ERROR_RADIO_OFF:
+			break;
+		}
+	}
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, AppWidget.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+
+            views.setTextViewText(R.id.tvWidget, result);
+            // Tell the AppWidgetManager to perform an update on the current App Widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+	}
+	
+	}
+	
+}
