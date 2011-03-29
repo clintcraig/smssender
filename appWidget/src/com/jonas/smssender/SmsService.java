79b0a02d9a2a5d0431ecbcbc031e59fb12da48f9
PenguinMore <PenguinMore@60587213-1f97-928a-cde8-6f4b2d768771>
2011-03-29 04:40:45 +0000

    add some comments for appWidget update

--- appWidget/src/com/jonas/smssender/SmsService.java
+++ appWidget/src/com/jonas/smssender/SmsService.java
@@ -14,10 +14,11 @@ import android.widget.RemoteViews;
 import android.widget.Toast;
 
 public class SmsService extends Service {
-    
+    //some variable for update appWidget
 	private static AppWidgetManager appWidgetMannager;
 	private static RemoteViews remoteViews;
 	private static ComponentName componentName;
+
 	@Override
 	public IBinder onBind(Intent arg0) {
 		// TODO Auto-generated method stub
@@ -33,7 +34,7 @@ public class SmsService extends Service {
 	}
 
 	public BroadcastReceiver smsReciever = new BroadcastReceiver() {
-		
+
 		@Override
 		public void onReceive(Context context, Intent intent) {
 			// TODO Auto-generated method stub
@@ -43,12 +44,16 @@ public class SmsService extends Service {
 			case Activity.RESULT_OK:
 				Toast.makeText(getApplicationContext(), "sms sent",
 						Toast.LENGTH_SHORT).show();
-				appWidgetMannager = AppWidgetManager.getInstance(SmsService.this);
-				remoteViews = new RemoteViews(SmsService.this.getPackageName(), R.layout.widget);
-                componentName = new ComponentName(SmsService.this,AppWidget.class);
-                remoteViews.setTextViewText(R.id.tvWidget, "发送成功");
-                appWidgetMannager.updateAppWidget(componentName, remoteViews);
-				
+				//get appWidgetMannager/remoteViews/componentName for update AppWidget
+				appWidgetMannager = AppWidgetManager
+						.getInstance(SmsService.this);
+				remoteViews = new RemoteViews(SmsService.this.getPackageName(),
+						R.layout.widget);
+				componentName = new ComponentName(SmsService.this,
+						AppWidget.class);
+				remoteViews.setTextViewText(R.id.tvWidget, "发送成功");
+				appWidgetMannager.updateAppWidget(componentName, remoteViews);
+
 				break;
 			case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
 
