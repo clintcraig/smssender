f83c9e366516bcd4b6f00003036097b11f3fbd11
PenguinMore <PenguinMore@60587213-1f97-928a-cde8-6f4b2d768771>
2011-03-29 09:33:28 +0000

    add a button user can click open Send Activity

--- trunk/appWidget/src/com/jonas/smssender/AppWidget.java
+++ trunk/appWidget/src/com/jonas/smssender/AppWidget.java
@@ -25,12 +25,13 @@ public class AppWidget extends AppWidgetProvider {
             int appWidgetId = appWidgetIds[i];
 
             // Create an Intent to launch ExampleActivity
-            Intent intent = new Intent(context, AppWidget.class);
+            Intent intent = new Intent(context, Send.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
             // Get the layout for the App Widget and attach an on-click listener to the button
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-
+            // set OnClickPendingIntent and TextViewText
+            views.setOnClickPendingIntent(R.id.btnWidget, pendingIntent);
             views.setTextViewText(R.id.tvWidget, result);
             // Tell the AppWidgetManager to perform an update on the current App Widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
