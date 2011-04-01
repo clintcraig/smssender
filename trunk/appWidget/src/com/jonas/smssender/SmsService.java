b6fb46b3a9ef9597be0c3e2d19b64b0a9bc172ee
PenguinMore <PenguinMore@60587213-1f97-928a-cde8-6f4b2d768771>
2011-04-01 02:43:02 +0000

    change Appwidget update logic emulator work fine,but real device fail

--- trunk/appWidget/src/com/jonas/smssender/SmsService.java
+++ trunk/appWidget/src/com/jonas/smssender/SmsService.java
@@ -16,10 +16,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class SmsService extends Service {
-    //some variable for update appWidget
+	// some variable for update appWidget
 	private static AppWidgetManager appWidgetMannager;
 	private static RemoteViews remoteViews;
 	private static ComponentName componentName;
+	/*
+	 * define and init orgMsg/smsNumber for save PhoneNumber and Message
+	 */
+	private static String orgMsg;
+	private static String smsNumber;
 
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -31,8 +36,11 @@ public class SmsService extends Service {
 	public void onCreate() {
 		// TODO Auto-generated method stub
 		super.onCreate();
+
+		orgMsg = "";
+		smsNumber = "";
 		IntentFilter receiverFilter = new IntentFilter("SMS");
-		//register smsReciever
+		// register smsReciever
 		registerReceiver(smsReciever, receiverFilter);
 	}
 
@@ -40,30 +48,39 @@ public class SmsService extends Service {
 	public void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
-		//unregister smsReciever
+		// unregister smsReciever
 		unregisterReceiver(smsReciever);
 	}
 
+	/*
+	 * Handler PendingIntent got SmsNumber from it,then update appWidget
+	 */
 	public BroadcastReceiver smsReciever = new BroadcastReceiver() {
 
 		@Override
 		public void onReceive(Context context, Intent intent) {
 			int resultCode = getResultCode();
-			String smsNumber = "";
+
 			Bundle bundle = intent.getExtras();
-			smsNumber = bundle.getString("SmsNumber");
+
 			switch (resultCode) {
 			case Activity.RESULT_OK:
-				Toast.makeText(getApplicationContext(), smsNumber+"发送成功！",
+
+				// get SmsNumber and build OrgMsg
+				smsNumber = bundle.getString("SmsNumber");
+				orgMsg += "\n" + smsNumber + "发送成功";
+
+				Toast.makeText(getApplicationContext(), smsNumber + "发送成功！",
 						Toast.LENGTH_SHORT).show();
-				//get appWidgetMannager/remoteViews/componentName for update AppWidget
+				// get appWidgetMannager/remoteViews/componentName for update
+				// AppWidget
 				appWidgetMannager = AppWidgetManager
 						.getInstance(SmsService.this);
 				remoteViews = new RemoteViews(SmsService.this.getPackageName(),
 						R.layout.widget);
 				componentName = new ComponentName(SmsService.this,
 						AppWidget.class);
-				remoteViews.setTextViewText(R.id.tvWidget,smsNumber+"发送成功！");
+				remoteViews.setTextViewText(R.id.tvWidget, orgMsg);
 				appWidgetMannager.updateAppWidget(componentName, remoteViews);
 
 				break;
